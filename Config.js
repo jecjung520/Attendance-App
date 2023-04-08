00e66fffc00115851efc938a0dad5b55ee7e17db
Eunchong Jung <jecjung520@gmail.com>
2023-04-08 23:20:54 +0800

    Deleted firebase key

--- Config.js
+++ Config.js
@@ -11,15 +11,7 @@ import 'firebase/compat/database';
 // import {...} from "firebase/storage";
 
 // Initialize Firebase
-const firebaseConfig = {
-    apiKey: "AIzaSyCgps1SF0ipokWDI5unS1DSdsYIWI3Qjqg",
-    authDomain: "attendance-app-99c42.firebaseapp.com",
-    projectId: "attendance-app-99c42",
-    storageBucket: "attendance-app-99c42.appspot.com",
-    messagingSenderId: "485253299053",
-    appId: "1:485253299053:web:990acfe835da16708a3864",
-    measurementId: "G-8YL48XW4S8"
-};
+
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
